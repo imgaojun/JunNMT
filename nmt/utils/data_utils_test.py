593593a879e764453af29611a747e14a6d1968b6
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-10 21:41:28 +0800

    update

--- nmt/utils/data_utils_test.py
+++ nmt/utils/data_utils_test.py
@@ -4,6 +4,7 @@ from data_utils import NMTDataSet
 dataset = NMTDataSet('/home/xiapeng/python/process_data/res/deve_src_file','/home/xiapeng/python/process_data/res/deve_tgt_file',10)
 
 while True:
-    print(dataset.iterator)
-except StopIteration:
-    print('end of epoch')
+    try:
+        print(dataset.iterator)
+    except StopIteration:
+        print('end of epoch')
