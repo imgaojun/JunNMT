5cba9be9e8302e5823c3a45144ed32538be5c5d0
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-11 08:31:55 +0800

    fix

--- nmt/utils/data_utils_test.py
+++ nmt/utils/data_utils_test.py
@@ -1,9 +1,9 @@
-from data_utils import NMTDataSet
+from data_utils import TrainDataSet
 import vocab_utils
 src_vocab_table = vocab_utils.VocabTable('/home/xiapeng/python/process_data/res/vocab.txt')
 tgt_vocab_table = vocab_utils.VocabTable('/home/xiapeng/python/process_data/res/vocab.txt')
 
-dataset = NMTDataSet('/home/xiapeng/python/process_data/res/deve_src_file','/home/xiapeng/python/process_data/res/deve_tgt_file',10,src_vocab_table,tgt_vocab_table)
+dataset = TrainDataSet('/home/xiapeng/python/process_data/res/deve_src_file','/home/xiapeng/python/process_data/res/deve_tgt_file',10,src_vocab_table,tgt_vocab_table)
 
 while True:
     try:
