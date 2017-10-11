b02b6ca10f8dd4b2ce8d916e9689a0bfb0b3bedd
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-11 16:20:58 +0800

    update

--- nmt/utils/data_utils_test.py
+++ nmt/utils/data_utils_test.py
@@ -1,5 +1,7 @@
 from data_utils import NMTDataSet
 
+src_vocab_table = vocab_utils.VocabTable(hparams['src_vocab_file'],hparams['src_vocab_size'])
+tgt_vocab_table = vocab_utils.VocabTable(hparams['tgt_vocab_file'],hparams['tgt_vocab_size'])
 
 dataset = NMTDataSet('/home/xiapeng/python/process_data/res/deve_src_file','/home/xiapeng/python/process_data/res/deve_tgt_file',10)
 
