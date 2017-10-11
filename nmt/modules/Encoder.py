781854dc85064099d9eed64a49a2015b82be3374
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-11 19:46:37 +0800

    update

--- nmt/modules/Encoder.py
+++ nmt/modules/Encoder.py
@@ -29,7 +29,7 @@ class EncoderGRU(nn.Module):
         return outputs, hidden
 
 class EncoderSRU(nn.Module):
-    def __init__(self, embeddings, input_size, hidden_size, num_layers=1, dropout=0.1, bidirectional=True):
+    def __init__(self, embeddings, input_size, hidden_size, num_layers=1, dropout=0.1, bidirectional=False):
         super(EncoderSRU, self).__init__()
         
         self.hidden_size = hidden_size
@@ -53,5 +53,5 @@ class EncoderSRU(nn.Module):
         embeded = self.embeddings(rnn_input)
         outputs, hidden = self.rnn(embeded)
         # outputs, output_lengths = torch.nn.utils.rnn.pad_packed_sequence(outputs) # unpack (back to padded)
-        outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:] # Sum bidirectional outputs
+        # outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:] # Sum bidirectional outputs
         return outputs, hidden
