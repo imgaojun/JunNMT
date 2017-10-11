5a4be5b5a7b33eba52d35272bc917d9173e00f24
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-11 09:45:13 +0800

    fix

--- nmt/Criteria.py
+++ nmt/Criteria.py
@@ -16,6 +16,7 @@ class Criteria(nn.Module):
         if max_len is None:
             max_len = sequence_length.data.max()
         batch_size = sequence_length.size(0)
+        print(batch_size)
         seq_range = torch.arange(0, max_len).long()
         seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
         seq_range_expand = Variable(seq_range_expand)
