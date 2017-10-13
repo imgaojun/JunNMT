599360fcf27659dc7051a575a7432fa0e98fca9a
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-13 22:02:42 +0800

    update

---
+++ scripts/train.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=0
+python3 ../train.py \
+    --config ../config.yml
---
+++ scripts/trainslate.sh
@@ -0,0 +1,7 @@
+export CUDA_VISIBLE_DEVICES=0
+python3 ../translate.py \
+    --config ../config.yml \
+    --src_in \
+    --tgt_out \
+    --model \
+                        --
