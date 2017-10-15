0f51f93941a26063f0b89abb3443641e41e16d7f
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-15 21:29:28 +0800

    fix

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -1,7 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 python3 ../translate.py \
     --config ../config.yml \
-    --src_in \
-    --tgt_out \
-    --model \
-                        --
+    --src_in /home/xiapeng/python/process_data/res/deve_src_file \
+    --tgt_out ./test_out \
+    --model /home/xiapeng/gaojun/JunNMT_test/gru_test1/epoch5/model.pkl
