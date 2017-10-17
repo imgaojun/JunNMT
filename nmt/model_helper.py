666d5b0e59fb6146eca92ade1219ccded2770ce0
gaojun4ever <gaojun4ever@hotmail.com>
2017-10-17 20:58:59 +0800

    update

--- nmt/model_helper.py
+++ nmt/model_helper.py
@@ -19,43 +19,56 @@ def create_emb_for_encoder_and_decoder(share_embedding,
 
 
 def create_encoder(hparams, embedding):
+ 
+    embedding_size = hparams['embedding_size']
+    hidden_size = hparams['hidden_size']
+    num_layers = hparams['num_layers']
+    dropout = hparams['dropout']
+    bidirectional = hparams['bidirectional']    
+
     if hparams['rnn_type'] == 'SRU':
         encoder = EncoderSRU(embedding,
-                            hparams['embedding_size'],
-                            hparams['hidden_size'],
-                            hparams['num_layers'],
-                            hparams['dropout'],
-                            hparams['bidirectional'])
+                            embedding_size,
+                            hidden_size,
+                            num_layers,
+                            dropout,
+                            bidirectional)
 
     elif hparams['rnn_type'] == 'GRU':
         encoder = EncoderGRU(embedding,
-                            hparams['embedding_size'],
-                            hparams['hidden_size'],
-                            hparams['num_layers'],
-                            hparams['dropout'],
-                            hparams['bidirectional'])
+                            embedding_size,
+                            hidden_size,
+                            num_layers,
+                            dropout,
+                            bidirectional)
                            
     return encoder
 
 def create_decoder(hparams, embedding, tgt_vocab_size):
+    atten_model = hparams['atten_model']
+    embedding_size = hparams['embedding_size']
+    if hparams['bidirectional']:      
+        hidden_size = hparams['hidden_size']*2
+    else:
+        hidden_size = hparams['hidden_size']
+    num_layers = hparams['num_layers']
+    dropout = hparams['dropout']    
     if hparams['rnn_type'] == 'SRU':
-        decoder = AttnDecoderSRU(hparams['atten_model'],
+        decoder = AttnDecoderSRU(atten_model,
                                 embedding,
-                                hparams['embedding_size'],
-                                hparams['hidden_size'],
+                                embedding_size,
+                                hidden_size,
                                 tgt_vocab_size,
-                                hparams['num_layers'],
-                                hparams['dropout'],
-                                hparams['bidirectional'])
+                                num_layers,
+                                dropout)
     elif hparams['rnn_type'] == 'GRU':
-        decoder = AttnDecoderGRU(hparams['atten_model'],
+        decoder = AttnDecoderGRU(atten_model,
                                 embedding,
-                                hparams['embedding_size'],
-                                hparams['hidden_size'],
+                                embedding_size,
+                                hidden_size,
                                 tgt_vocab_size,
-                                hparams['num_layers'],
-                                hparams['dropout'],
-                                hparams['bidirectional'])
+                                num_layers,
+                                dropout)
                               
 
     return decoder
