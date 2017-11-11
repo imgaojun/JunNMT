0b66d5750b4ee9e8f9b0495c0573cf210137b642
gaojun4ever <gaojun4ever@hotmail.com>
2017-11-11 16:54:23 +0800

    update

--- nmt/modules/Decoder.py
+++ nmt/modules/Decoder.py
@@ -41,10 +41,10 @@ class AttnDecoderRNN(DecoderBase):
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.dropout = nn.Dropout(dropout)  
-        self.hidden_init_net = nn.Sequential(
-                            nn.Linear(hidden_size, hidden_size),
-                            nn.Tanh()
-                        )
+        # self.hidden_init_net = nn.Sequential(
+        #                     nn.Linear(hidden_size, hidden_size),
+        #                     nn.Tanh()
+        #                 )
         if rnn_type == "SRU":
             self.rnn = SRU(
                     input_size=input_size,
@@ -83,82 +83,81 @@ class AttnDecoderRNN(DecoderBase):
 
     def init_decoder_state(self, enc_hidden):
         if not isinstance(enc_hidden, tuple):  # GRU
-            # h = self.merge_net(enc_hidden)
-            h = self.hidden_init_net(enc_hidden)
-            # h = enc_hidden
+            # h = self.hidden_init_net(enc_hidden)
+            h = enc_hidden
             
         else:  # LSTM
             h = enc_hidden
         return h
 
 
-class InputFeedDecoder(DecoderBase):
-    def __init__(self, rnn_type, attn_model, input_size, 
-                hidden_size, num_layers=1, dropout=0.1):
-        super(InputFeedDecoder, self).__init__()    
-        # Basic attributes.
-        self.rnn_type = rnn_type
-        self.attn_model = attn_model
-        self.num_layers = num_layers
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.dropout = nn.Dropout(dropout)  
+# class InputFeedDecoder(DecoderBase):
+#     def __init__(self, rnn_type, attn_model, input_size, 
+#                 hidden_size, num_layers=1, dropout=0.1):
+#         super(InputFeedDecoder, self).__init__()    
+#         # Basic attributes.
+#         self.rnn_type = rnn_type
+#         self.attn_model = attn_model
+#         self.num_layers = num_layers
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.dropout = nn.Dropout(dropout)  
 
-        if rnn_type == "SRU":
-            self.rnn = SRU(
-                    input_size=input_size,
-                    hidden_size=hidden_size,
-                    num_layers=num_layers,
-                    dropout=dropout)
-        else:
-            self.rnn = getattr(nn, rnn_type)(
-                    input_size=self._input_size,
-                    hidden_size=hidden_size,
-                    num_layers=num_layers,
-                    dropout=dropout)              
+#         if rnn_type == "SRU":
+#             self.rnn = SRU(
+#                     input_size=input_size,
+#                     hidden_size=hidden_size,
+#                     num_layers=num_layers,
+#                     dropout=dropout)
+#         else:
+#             self.rnn = getattr(nn, rnn_type)(
+#                     input_size=self._input_size,
+#                     hidden_size=hidden_size,
+#                     num_layers=num_layers,
+#                     dropout=dropout)              
 
-        if self.attn_model != 'none':
-            self.attn = GlobalAttention(hidden_size, attn_model)
+#         if self.attn_model != 'none':
+#             self.attn = GlobalAttention(hidden_size, attn_model)
 
-    def forward(self, input, context, state):
-        outputs = []
-        output = self.init_input_feed(context).squeeze(0)
-        emb  = input
-        hidden = state
+#     def forward(self, input, context, state):
+#         outputs = []
+#         output = self.init_input_feed(context).squeeze(0)
+#         emb  = input
+#         hidden = state
 
-        for i, emb_t in enumerate(emb.split(1)):
-            emb_t = emb_t.squeeze(0)
-            emb_t = torch.cat([emb_t, output], 1)  
-            emb_t = emb_t.unsqueeze(0)
+#         for i, emb_t in enumerate(emb.split(1)):
+#             emb_t = emb_t.squeeze(0)
+#             emb_t = torch.cat([emb_t, output], 1)  
+#             emb_t = emb_t.unsqueeze(0)
 
-            rnn_output, hidden = self.rnn(emb_t, hidden)
-            attn_output, attn = self.attn(
-                                rnn_output.transpose(0, 1).contiguous(),  # (output_len, batch, d)
-                                context.transpose(0, 1)                   # (contxt_len, batch, d)
-                            )    
+#             rnn_output, hidden = self.rnn(emb_t, hidden)
+#             attn_output, attn = self.attn(
+#                                 rnn_output.transpose(0, 1).contiguous(),  # (output_len, batch, d)
+#                                 context.transpose(0, 1)                   # (contxt_len, batch, d)
+#                             )    
 
-            output = self.dropout(attn_output)
-            outputs += [output]
-        return outputs, hidden
+#             output = self.dropout(attn_output)
+#             outputs += [output]
+#         return outputs, hidden
 
-    def init_decoder_state(self, enc_hidden):
-        if not isinstance(enc_hidden, tuple):  # GRU
-            # h = self.merge_net(enc_hidden)
-            h = self.hidden_init_net(enc_hidden)
-        else:  # LSTM
-            h = enc_hidden
-        return h
+#     def init_decoder_state(self, enc_hidden):
+#         if not isinstance(enc_hidden, tuple):  # GRU
+#             # h = self.merge_net(enc_hidden)
+#             h = self.hidden_init_net(enc_hidden)
+#         else:  # LSTM
+#             h = enc_hidden
+#         return h
 
-    def init_input_feed(self, context):
-        batch_size = context.size(1)
-        hidden_size = self.hidden_size
-        h_size = (batch_size, hidden_size)
-        return Variable(context.data.new(*h_size).zero_(),
-                                   requires_grad=False).unsqueeze(0)
+#     def init_input_feed(self, context):
+#         batch_size = context.size(1)
+#         hidden_size = self.hidden_size
+#         h_size = (batch_size, hidden_size)
+#         return Variable(context.data.new(*h_size).zero_(),
+#                                    requires_grad=False).unsqueeze(0)
 
-    @property
-    def _input_size(self):
-        """
-        Using input feed by concatenating input with attention vectors.
-        """
-        return self.input_size + self.hidden_size
+#     @property
+#     def _input_size(self):
+#         """
+#         Using input feed by concatenating input with attention vectors.
+#         """
+#         return self.input_size + self.hidden_size
