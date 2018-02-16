448371c99f355ab10092e6f4d490ed548247b38e
gaojun4ever <gaojun4ever@hotmail.com>
2018-02-16 20:05:24 +0800

    update

--- translate.py
+++ translate.py
@@ -11,16 +11,16 @@ def translate_sentence(translator, sent, fields, use_cuda):
                                     use_cuda)
 
     ret = translator.translate_batch(src_input_var,src_input_lengths)
-    print(ret['predictions'])
-    # all_hyp_inds = [[x[0] for x in hyp] for hyp in hypotheses]
+    all_hyp_inds = ret['predictions'][0]
+    all_hyp_inds = [[x[0] for x in hyp] for hyp in hypotheses]
     
-    # all_hyp_words = [nmt.data_utils.indices2words(idxs,fields['tgt'].vocab.itos) for idxs in all_hyp_inds]
+    all_hyp_words = [nmt.data_utils.indices2words(idxs,fields['tgt'].vocab.itos) for idxs in all_hyp_inds]
     
     
-    # sentence_out = ' '.join(all_hyp_words[0])
-    # # sentence_out = sentence_out.replace(' <unk>','')
-    # sentence_out = sentence_out.replace(' </s>','')
-    return ret
+    sentence_out = ' '.join(all_hyp_words[0])
+    sentence_out = sentence_out.replace(' <unk>','')
+    sentence_out = sentence_out.replace(' </s>','')
+    return sentence_out
 
 def translate_file(translator, src_fin, tgt_fout, fields, use_cuda):
 
