539b5b01c4919829d0533bb05a8bf70b29e974bb
gaojun4ever <gaojun4ever@hotmail.com>
2018-02-17 00:49:41 +0800

    update

--- translate.py
+++ translate.py
@@ -56,6 +56,8 @@ def translate_file(translator, src_fin, tgt_fout, fields, use_cuda, dump_beam=No
             beam_trace = translator.beam_accum
             print(len(beam_trace['predicted_ids'][0]))
             print(len(beam_trace['predicted_ids'][0][0]))
+            print(len(beam_trace['scores'][0]))
+            print(len(beam_trace['scores'][0][0]))
             with codecs.open(dump_beam,'w',encoding='utf8') as f:
                 f.write(json.dumps(beam_trace))
 
