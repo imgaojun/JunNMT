681faee29202e028cfa49e0693af702117250a31
gaojun4ever <gaojun4ever@hotmail.com>
2018-05-30 21:12:54 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,4 +3,8 @@ python3 ${NMT_DIR}/train.py \
     -gpuid 0 \
     -config ./config.yml \
     -nmt_dir ${NMT_DIR} \
-    -data demo
+    -train_src /home/gaojun4ever/Documents/Projects/mt-exp/data/dev/nist02.cn \
+    -train_tgt /home/gaojun4ever/Documents/Projects/mt-exp/data/dev/nist02.en0 \
+    -valid_src /home/gaojun4ever/Documents/Projects/mt-exp/data/dev/nist02.cn \
+    -valid_tgt /home/gaojun4ever/Documents/Projects/mt-exp/data/dev/nist02.en0 \
+    -vocab ./demo.vocab.pkl
