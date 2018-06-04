db1223830ae53129254b2d628058828984ea98c3
gaojun4ever <gaojun4ever@hotmail.com>
2018-06-04 11:14:21 +0800

    update

--- translate.py
+++ translate.py
@@ -47,52 +47,6 @@ def translate_file(translator,
             with codecs.open(dump_beam,'w',encoding='utf8') as f:
                 json.dump(beam_trace,f)
 
-# def translate_sentence(translator, sent, fields, use_cuda):
-#     src_input_var, src_input_lengths= \
-#         nmt.data_utils.batch_seq2var([sent],
-#                                     fields['src'].vocab.stoi,
-#                                     use_cuda)
-
-#     ret = translator.translate_batch(src_input_var,src_input_lengths)
-#     return ret
-
-
-
-
-# def translate_file(translator, src_fin, tgt_fout, fields, use_cuda, dump_beam=None):
-
-#     def get_sentence(sent_list):
-#         all_hyp_inds = sent_list#ret['predictions'][0]
-        
-#         all_hyp_words = [nmt.data_utils.indices2words(idxs,fields['tgt'].vocab.itos) for idxs in all_hyp_inds]
-#         sentence_out = ' '.join(all_hyp_words[0])
-#         sentence_out = sentence_out.replace(' <unk>','')
-#         sentence_out = sentence_out.replace(' </s>','')
-
-#         return sentence_out        
-
-
-#     print('start translating ...')
-#     with codecs.open(src_fin, 'r', encoding='utf8', errors='ignore') as src_file,\
-#             codecs.open(tgt_fout, 'wb', encoding='utf8') as tgt_file:
-#         src_lines = src_file.readlines()
-#         process_bar = nmt.misc_utils.ShowProcess(len(src_lines))
-#         for line in src_lines:
-#             src_seq = line.strip()
-#             ret = translate_sentence(translator, src_seq, fields, use_cuda)
-#             sentence_out = get_sentence(ret['predictions'][0])
-
-#             tgt_file.write(sentence_out+'\n')
-
-#             process_bar.show_process()
-#         if dump_beam:
-#             print('dump beam ....')
-#             beam_trace = translator.beam_accum
-#             with codecs.open(dump_beam,'w',encoding='utf8') as f:
-#                 json.dump(beam_trace,f)
-
-
-
 def main():
 
     parser = argparse.ArgumentParser()
@@ -132,17 +86,6 @@ def main():
     if use_cuda:
         model = model.cuda()    
 
-    # translator = nmt.Translator(model=model, 
-    #                             fields=fields,
-    #                             beam_size=opt.beam_size, 
-    #                             n_best=1,
-    #                             max_length=opt.decode_max_length,
-    #                             global_scorer=None,
-    #                             cuda=use_cuda,
-    #                             beam_trace=True if args.dump_beam else False)
-
-    # translate_file(translator, args.src_in, args.tgt_out, fields, use_cuda, args.dump_beam)
-    
     translator = nmt.Translator(model=model, 
                                 fields=fields,
                                 beam_size=opt.beam_size, 
